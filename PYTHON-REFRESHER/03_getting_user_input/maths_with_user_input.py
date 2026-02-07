764a8d39c035ec1f4ca265ff7bbb9dfc5f675ce5
Farhan <ahmad.farohan@gmail.com>
2026-02-07 19:22:27 +0700

    add user input

---
+++ PYTHON-REFRESHER/03_getting_user_input/maths_with_user_input.py
@@ -0,0 +1,4 @@
+size_input = input("How big is your house? (in square feet): ")
+square_feet = int(size_input)
+square_metres = square_feet / 10.8
+print(f"Your house is {square_metres:.2f} square metres.")
