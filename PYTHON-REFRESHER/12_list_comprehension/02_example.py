e2d8bdd82563debd3d9287fc250e84d8e734b8fa
Farhan <ahmad.farohan@gmail.com>
2026-02-08 07:24:49 +0700

    add list comprehension

---
+++ PYTHON-REFRESHER/12_list_comprehension/02_example.py
@@ -0,0 +1,7 @@
+friends = ["Sam", "Samantha", "Saurabh"]
+starts_s = friends
+
+print(friends)
+print(starts_s)
+print(friends is starts_s)
+print("friends: ", id(friends), "starts_s: ", id(starts_s))
